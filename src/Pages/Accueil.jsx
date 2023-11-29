873d3da6e1c2d0deb556543c42f5e1650ae44a8a
TondeJunior <zogningfranc@gmail.com>
2023-11-29 03:45:57 +0000

    En cours

--- src/Pages/Accueil.jsx
+++ src/Pages/Accueil.jsx
@@ -2,13 +2,76 @@ import React, { useEffect } from "react";
 import Header from "../components/Header";
 import "./../assets/style/home.css"
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
-import liv1 from "../assets/liv1.jpg"
 import liv2 from "../assets/liv2.jpg"
-import liv3 from "../assets/liv3.jpg"
-import liv4 from "../assets/liv4.jpg"
-import liv5 from "../assets/liv5.jpeg"
+import { Link } from "react-router-dom";
 
-let liste = [liv1,liv2,liv3,liv4,liv5]
+let liste = [
+    {
+        notice_id: 161,
+        typdoc: "a",
+        tit1: "The Mind of Jesus",
+        ed1_id: 70,
+        year: "",
+        mention_edition: "",
+        npages: "170",
+        n_resume: "",
+        origine_catalogage: 1,
+        statut: 1,
+        indexation_lang: "en_UK"
+    },
+    {
+        "notice_id": 385,
+        "typdoc": "a",
+        "tit1": "The unlighted lustre",
+        "ed1_id": 107,
+        "year": "",
+        "mention_edition": "",
+        "npages": "278",
+        "n_resume": "",
+        "origine_catalogage": 1,
+        "statut": 1,
+        "indexation_lang": ""
+    },
+    {
+        "notice_id": 386,
+        "typdoc": "a",
+        "tit1": "the harpercollins study bible",
+        "ed1_id": 167,
+        "year": "",
+        "mention_edition": "",
+        "npages": "2337",
+        "n_resume": "",
+        "origine_catalogage": 1,
+        "statut": 1,
+        "indexation_lang": ""
+    },
+    {
+        "notice_id": 387,
+        "typdoc": "a",
+        "tit1": "the life of continual rejoicing",
+        "ed1_id": 127,
+        "year": "",
+        "mention_edition": "",
+        "npages": "127",
+        "n_resume": "",
+        "origine_catalogage": 1,
+        "statut": 1,
+        "indexation_lang": ""
+    },
+    {
+        "notice_id": 392,
+        "typdoc": "a",
+        "tit1": "1 CORINTHIANS",
+        "ed1_id": 127,
+        "year": "",
+        "mention_edition": "",
+        "npages": "112",
+        "n_resume": "",
+        "origine_catalogage": 1,
+        "statut": 1,
+        "indexation_lang": ""
+    },
+]
 
 export default function Accueil() {
 
@@ -30,7 +93,14 @@ export default function Accueil() {
                     </div>
                     <div className="slider">
                         <div className="content_slide">
-                            {liste.map((livre)=><div className="item"><img src={livre} alt="livre" /></div>)}
+                            {liste.map((livre) => <div className="item">
+                                    <img src={liv2} alt="livre" />
+                                    <Link to={"/home/livre/"+livre.notice_id}>
+                                        <p>
+                                        {livre.tit1}</p>
+                                    </Link>
+                                </div>
+                            )}
                         </div>
                     </div>
                     <div className="right">
--- src/Pages/admin/AdminDashboad.jsx
+++ src/Pages/admin/AdminDashboad.jsx
@@ -1,24 +1,49 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useSyncExternalStore } from "react";
 import ContentViewer from "../../components/admin/ContentViewer";
 import DashHeader from "../../components/admin/DashHeader";
 import SideBar from "../../components/admin/SideBar";
 import "./../../assets/style/dashboard.css"
 import liv1 from "../../assets/liv1.jpg"
 import liv2 from "../../assets/liv2.jpg"
-import liv3 from "../../assets/liv3.jpg"
-import liv4 from "../../assets/liv4.jpg"
-import liv5 from "../../assets/liv5.jpeg"
 
-const livres = [liv1,liv2,liv3,liv4,liv5,liv1,liv2,liv3,liv4,liv5]
+const livres = [liv1, liv2]
 
 export default function AdminDashboad() {
     const [dimension, setDimension] = useState();
     const [toggleVal, setToggleVal] = useState(true);
-    
+    const [data, setData] = useState([
+        {
+            "notice_id": 79,
+            "typdoc": "a",
+            "tit1": "Les loups",
+            "ed1_id": 16,
+            "year": "2006",
+            "mention_edition": "",
+            "npages": "1 vol. (non paginé [31] p.)",
+            "n_resume": " ",
+            "origine_catalogage": 7,
+            "statut": 1,
+            "indexation_lang": ""
+        },
+        {
+            "notice_id": 82,
+            "typdoc": "a",
+            "tit1": "La sagesse du bibliothécaire",
+            "ed1_id": 19,
+            "year": "2004",
+            "mention_edition": "",
+            "npages": "109 p.",
+            "n_resume": "",
+            "origine_catalogage": 7,
+            "statut": 1,
+            "indexation_lang": ""
+        },
+    ])
+
 
     useEffect(() => {
         setDimension(window.innerWidth);
-    }, []);
+    }, [data]);
 
     return (
         <div className='admindashboard'>
@@ -26,7 +51,26 @@ export default function AdminDashboad() {
                 <DashHeader />
                 <div className="conten">
                     <div className="livres">
-                        {livres.map((livre)=><div className="item"><img src={livre} alt="" /></div>)}
+                        {data.map((livre) => <div className="item">
+                            <img src={liv2} alt="" />
+                            <div className="texte">
+                                <div className="title">{livre.tit1}</div>
+                                <div className="info">
+                                    <p>
+                                        Auteur : <span>{ }</span>
+                                    </p>
+                                    <p>
+                                        Année : <span>{livre.year}</span>
+                                    </p>
+                                    <p>
+                                        Nombre de pages : <span>{livre.npages}</span>
+                                    </p>
+                                    <p>
+                                        Type : <span>Livre</span>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>)}
                     </div>
                 </div>
                 <SideBar dimension={dimension} status={toggleVal} />
